b6ad019d44ca34f7f7ea29a223446906e8be775e
Johns23-e <johnroqueqyt@gmail.com>
2025-09-17 11:54:22 +0800

    chore: initial commit

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
+context = {'title': 'Django Project'}
 # Create your views here.
 def base(request):
-    return render(request, 'base.html')
+    return render(request, 'base.html', context)
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', context)
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', context)
